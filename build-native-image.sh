edc7d43f161aeb6b84a4a5f2ad550fdd5c460699
Iván López <lopez.ivan@gmail.com>
2019-02-04 10:03:41 +0100

    Use service loader

--- build-native-image.sh
+++ build-native-image.sh
@@ -4,10 +4,11 @@ native-image --no-server \
              --class-path build/libs/micronaut-security-jwt-graal-0.1-all.jar \
              -H:ReflectionConfigurationFiles=build/reflect.json \
              -H:EnableURLProtocols=http \
-             -H:IncludeResources="logback.xml|application.yml|META-INF/services/*.*" \
+             -H:IncludeResources="logback.xml|application.yml" \
              -H:Name=mn-security \
              -H:Class=example.micronaut.Application \
              -H:+ReportUnsupportedElementsAtRuntime \
              -H:+AllowVMInspection \
+             --allow-incomplete-classpath \
              --rerun-class-initialization-at-runtime='sun.security.jca.JCAUtil$CachedSecureRandomHolder,javax.net.ssl.SSLContext' \
              --delay-class-initialization-to-runtime=io.netty.handler.codec.http.HttpObjectEncoder,io.netty.handler.codec.http.websocketx.WebSocket00FrameEncoder,io.netty.handler.ssl.util.ThreadLocalInsecureRandom
