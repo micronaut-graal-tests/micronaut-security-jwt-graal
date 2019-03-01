0e7553bd8bf992b163f0fbc4334fc548371a5278
Iván López <lopez.ivan@gmail.com>
2019-03-01 12:32:15 +0100

    Update build.gradle and use BeanIntrospection

--- src/main/java/example/micronaut/MicronautSubstitutions.java
+++ src/main/java/example/micronaut/MicronautSubstitutions.java
@@ -1,13 +1,14 @@
+package example.micronaut;
+
 import com.oracle.svm.core.annotate.Alias;
 import com.oracle.svm.core.annotate.RecomputeFieldValue;
 import com.oracle.svm.core.annotate.RecomputeFieldValue.Kind;
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
-
 import io.netty.util.internal.logging.InternalLoggerFactory;
 import io.netty.util.internal.logging.JdkLoggerFactory;
 
-@TargetClass(io.netty.util.internal.logging.InternalLoggerFactory.class)
+@TargetClass(InternalLoggerFactory.class)
 final class Target_io_netty_util_internal_logging_InternalLoggerFactory {
     @Substitute
     private static InternalLoggerFactory newDefaultFactory(String name) {
@@ -17,7 +18,7 @@ final class Target_io_netty_util_internal_logging_InternalLoggerFactory {
 
 @TargetClass(className = "io.micronaut.caffeine.cache.UnsafeRefArrayAccess")
 final class Target_io_micronaut_caffeine_cache_UnsafeRefArrayAccess {
-    @Alias @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.ArrayIndexShift, declClass = Object[].class)
+    @Alias @RecomputeFieldValue(kind = Kind.ArrayIndexShift, declClass = Object[].class)
     public static int REF_ELEMENT_SHIFT;
 }
 
