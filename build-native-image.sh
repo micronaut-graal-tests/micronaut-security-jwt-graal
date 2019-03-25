ab380ac2b6a9bb14e0ff9a6ac0486ddf0b3f57c6
Iván López <lopez.ivan@gmail.com>
2019-03-25 12:40:56 +0100

    Fix fatjar name

--- build-native-image.sh
+++ build-native-image.sh
@@ -1,2 +1,2 @@
 ./gradlew assemble
-native-image --no-server --class-path build/libs/micronaut-security-jwt-graal-0.1-all.jar
+native-image --no-server --class-path build/libs/micronaut-security-jwt-graal-*.jar
