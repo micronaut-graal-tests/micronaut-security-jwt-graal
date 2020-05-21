de473416c4acd1350f8a6bd63f125270c1f8fea9
Sergio del Amo <sergio.delamo@softamo.com>
2020-05-21 07:07:01 +0200

    refactor: use constant

--- src/main/java/example/micronaut/HomeController.java
+++ src/main/java/example/micronaut/HomeController.java
@@ -5,11 +5,12 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
 import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
 
 import java.security.Principal;
 
-@Secured("isAuthenticated()")
 @Controller("/")
+@Secured(SecurityRule.IS_AUTHENTICATED)
 public class HomeController {
 
     @Get("/")
